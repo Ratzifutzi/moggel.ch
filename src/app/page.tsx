e1080ee12e94631c149a2052e411eb64675fa704
Ratzifutzi <71138238+Ratzifutzi@users.noreply.github.com>
2026-01-05 23:52:07 +0100

    Maintenance Screen added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
 	return (
-		<main>
-			<div>Hello world!</div>
+		<main className="grid min-h-screen grid-rows-[1fr_auto_1fr] place-items-center p-24">
+			<img
+				src="/assets/images/maintenance.png"
+				alt="Moggel Character hitting a Server"
+				className="mb-8 h-100"
+			/>
+			<div className="flex flex-col items-center">
+				<h1 className="text-center text-7xl font-bold">Maintenance</h1>
+				<p className="mt-6 max-w-prose text-center">
+					The website is currently under maintenance. Please check back later.
+				</p>
+			</div>
 		</main>
 	);
 }
