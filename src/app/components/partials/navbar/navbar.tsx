e86a490e556cfd8983ec0d52f73d9bde98efaf55
Joshua Schmidt <joshua@hyper-tech.ch>
2026-01-07 19:28:10 +0100

    Sidebar tests

---
+++ src/app/components/partials/navbar/navbar.tsx
@@ -0,0 +1,22 @@
+const Navbar = ({ children }: React.PropsWithChildren<{}>) => {
+	return (
+		<div className="flex h-full w-full flex-col gap-2">
+			<Item />
+			<Item />
+			<Item />
+			<Item />
+		</div>
+	);
+};
+
+const Item = () => {
+	return (
+		<div className="h-15 w-full bg-green-600">
+			<div className="h-15 w-15 bg-blue-500"></div>
+		</div>
+	);
+};
+
+Navbar.Item = Item;
+
+export default Navbar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Navbar from './components/partials/navbar/navbar';
 
 export const metadata: Metadata = {
 	title: 'Moggel',
@@ -16,10 +17,9 @@ export default function RootLayout({
 			<body>
 				<main className="flex h-screen w-screen items-center justify-center overflow-hidden">
 					<div className="flex h-screen w-screen flex-row gap-5 md:aspect-3/4 md:h-auto md:max-h-[80vh] md:w-[55%] md:max-w-225">
-						<div
-							className="hidden h-full w-80 bg-red-500 md:flex"
-							id="navbar"
-						></div>
+						<div className="hidden h-full w-65 md:flex" id="navbar">
+							<Navbar />
+						</div>
 
 						<div className="h-full w-full bg-black" id="content">
 							{children}
