04bfa05a3c249b623ab3cc34db50e2eb5a8623bd
Joshua Schmidt <joshua@hyper-tech.ch>
2026-01-07 18:52:15 +0100

    Changed scaling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,13 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body>
-				<main className="flex h-screen w-screen items-center justify-center">
-					<div className="relative h-screen w-screen max-w-300 md:aspect-3/4 md:h-auto md:max-h-[80vh] md:w-[40%] md:max-w-225">
+				<main className="flex h-screen w-screen items-center justify-center overflow-hidden">
+					<div className="flex h-screen w-screen flex-row gap-5 md:aspect-3/4 md:h-auto md:max-h-[80vh] md:w-[55%] md:max-w-225">
+						<div
+							className="hidden h-full w-80 bg-red-500 md:flex"
+							id="navbar"
+						></div>
+
 						<div className="h-full w-full bg-black" id="content">
 							{children}
 						</div>
