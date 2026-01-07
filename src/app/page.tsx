4508d69f75fcdc73d5f7434e12b59e255da1bb80
Joshua Schmidt <joshua@hyper-tech.ch>
2026-01-07 19:00:09 +0100

    New maintenance page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
 	return (
-		<main className="grid min-h-screen grid-rows-[1fr_auto_1fr] place-items-center p-24">
+		<main className="flex h-screen w-screen flex-col items-center justify-center">
 			<img
 				src="/assets/images/maintenance.png"
-				alt="Moggel Character hitting a Server"
-				className="mb-8 h-100"
+				alt="Maintenance Illustration"
+				className="mb-4 h-auto w-full max-w-md min-w-md object-contain"
 			/>
 			<div className="flex flex-col items-center">
 				<p className="mt-6 max-w-prose text-center text-lg">
