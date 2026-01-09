0cd7a5893357e72f250f5bcaf242205746f3f000
Ratzifutzi <71138238+Ratzifutzi@users.noreply.github.com>
2026-01-09 17:52:27 +0100

    Removed MAIN

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,18 +14,16 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body>
-				<main className="flex h-screen w-screen items-center justify-center overflow-hidden">
-					<div className="flex h-screen w-screen flex-row gap-5 md:max-h-[80vh] md:w-[85%] lg:aspect-3/4 lg:h-auto lg:w-[55%] lg:max-w-225">
-						<div className="hidden h-full w-65 md:flex" id="navbar">
-							<Navbar />
-						</div>
-
-						<div className="h-full w-full bg-black" id="content">
-							{children}
-						</div>
+			<body className="flex h-screen w-screen items-center justify-center overflow-hidden">
+				<div className="flex h-screen w-screen flex-row gap-5 md:max-h-[80vh] md:w-[85%] lg:aspect-3/4 lg:h-auto lg:w-[55%] lg:max-w-225">
+					<div className="hidden h-full w-65 md:flex" id="navbar">
+						<Navbar />
 					</div>
-				</main>
+
+					<div className="h-full w-full bg-black" id="content">
+						{children}
+					</div>
+				</div>
 			</body>
 		</html>
 	);
