2171626084f1a0a982a7b4d97db12962621475b2
Ratzifutzi <ratzifutzi@gmail.com>
2026-01-12 18:02:48 +0100

    Viewport changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import Navbar from './components/partials/navbar/navbar';
 
 export const metadata: Metadata = {
 	title: 'Moggel',
 	description: '',
+};
+export const viewport: Viewport = {
+	width: 'device-width',
+	initialScale: 1,
 };
 
 export default function RootLayout({
