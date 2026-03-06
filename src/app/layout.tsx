986429dd1dd7c3df4869f2638494d84369456107
Ratzifutzi <joshua@hyper-tech.ch>
2026-03-06 12:20:17 +0100

    Removed colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,12 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body>
-				<div className="fixed inset-0 flex items-center justify-center bg-red-600">
-					<div className="w-100% h-100% flex flex-row gap-5 bg-blue-500 md:max-h-[80svh] md:w-[85%] lg:aspect-3/4 lg:h-auto lg:w-[55%] lg:max-w-225">
+				<div className="fixed inset-0 flex items-center justify-center">
+					<div className="w-100% h-100% flex flex-row gap-5 md:max-h-[80svh] md:w-[85%] lg:aspect-3/4 lg:h-auto lg:w-[55%] lg:max-w-225">
 						<div className="w-65 md:flex" id="navbar">
 							<Navbar />
 						</div>
-						<div className="w-full bg-yellow-400" id="content">
+						<div className="w-full" id="content">
 							{children}
 						</div>
 					</div>
