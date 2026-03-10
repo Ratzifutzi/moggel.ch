8d67165673148f5584b5c98cdebfa69544aa7d8a
Ratzifutzi <joshua@hyper-tech.ch>
2026-03-10 15:40:56 +0100

    Added file transports for logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -10,7 +10,24 @@ class Logger {
 				winston.format.colorize(),
 				winston.format.simple(),
 			),
-			transports: [new winston.transports.Console()],
+			transports: [
+				new winston.transports.Console(),
+				new winston.transports.File({
+					filename: 'combined.log',
+					format: winston.format.json(),
+					dirname: 'logs',
+					maxFiles: 5,
+					maxsize: 5242880, // 5MB
+				}),
+				new winston.transports.File({
+					filename: 'logs/error.log',
+					level: 'error',
+					format: winston.format.json(),
+					dirname: 'logs',
+					maxFiles: 5,
+					maxsize: 5242880, // 5MB
+				}),
+			],
 		});
 
 		this.winston.info('Logger initialized');
